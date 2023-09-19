253c2436b174a5b95cba437b8a2b0ab4faf7f2cf
Scott Small <ssmall41@gmail.com>
2023-09-19 17:10:59 +0200

    Add extra modules

---
+++ experiment.py
@@ -0,0 +1,41 @@
+import numpy as np
+from numpy.linalg import norm
+
+from select_parties import select_parties_by_embeddings
+
+
+def run_trials(valid_parties_embeddings: list, num_parties: int, num_trials: int, eps: float) -> list:
+    """ Select a collection of num_parties, num_trials times. This returns the selected parties for
+    each trial, as well as the comparison matrix for each party.
+    :param valid_parties_embeddings: the embedding list for all valid parties
+    :param num_parties: the number of parties to select in each trial
+    :param num_trials: the number of trials
+    :param eps: the value of eps to use for the definition of close in each trial
+    :return: a list of tuples. Each tuple contains a list of select parties and the comparison
+    matrix.
+    """
+    trials = []
+    for t in range(num_trials):
+        print(f"Trial {t} #######")
+        selected_parties = select_parties_by_embeddings(valid_parties_embeddings, num_parties, eps)
+        comparison_matrix = generate_comparison_matrix(selected_parties)
+        
+        trials.append(([p[0] for p in selected_parties], comparison_matrix))
+    return trials
+
+
+def generate_comparison_matrix(selected_parties: list) -> np.ndarray:
+    """ Generates a matrix that shows the distance of each party in selected_parties from each
+    other, in terms of the 2-norm of their embeddings. This is helpful for analyses.
+    :param selected_parties: embedding list of the form [("job1,job2,job3,job4", embedding), (), ...]
+    :return: the matrix of distances of each job to each other job
+    """
+
+    comparison_matrix = np.zeros((len(selected_parties), len(selected_parties)), dtype=float)
+    
+    for row_idx, row_tuple in enumerate(selected_parties):
+        for col_idx, col_tuple in enumerate(selected_parties):
+            _, embedding_row = row_tuple
+            _, embedding_col = col_tuple
+            comparison_matrix[row_idx][col_idx] = norm(embedding_row - embedding_col, ord=2)
+    return comparison_matrix
---
+++ select_parties.py
@@ -0,0 +1,81 @@
+from copy import copy
+import numpy as np
+from numpy.linalg import norm
+from random import randint
+
+
+def select_parties_by_embeddings(valid_parties: list, num_parties: int = 10, eps: float = 1.0) -> list:
+    """ Given a selection of party embeddings, select num_parties which are intended to be played. This
+    uses the embeddings in the selection process. The first party is selected at random. Further parties
+    are selected so that they more than eps away (in 2-norm) from any other selected party.
+    While selecting parties, it's possible that all previously selected parties cover the remaining parties.
+    If this happens, eps is automatically reduced. So the eps provided can be thought of as a "starting" value.
+    
+    In general, the higher the value of eps, the more diversity in selected parties.
+
+    :param valid_parties: a list of party embeddings. Must be of the form 
+    [("job1,job2,job3,job4", embedding), (), ...]
+    :param num_parties: the number of parties to select
+    :param eps: the distance all selected parties must be from each other, to start
+    :return: the list of selected parties
+    """
+
+    available_parties = copy(valid_parties)
+    selected_parties = []
+    unavailable_parties = []
+
+    if num_parties > len(valid_parties):
+        num_parties = len(valid_parties)
+        print(f"Notice: num_parties was larger than the number of valid parties. Setting num_parties to {num_parties}.")
+    
+    for idx_party in range(0, num_parties):
+
+        # Select a party
+        chosen_party_idx = randint(0, len(available_parties)-1)
+        selected_parties.append(available_parties[chosen_party_idx])
+        available_parties[chosen_party_idx] = available_parties[-1]
+        available_parties.pop()
+        
+        # Organize the available parties by whether they are close to the chosen party or not
+        close_parties, far_parties = organize_parties(selected_parties[-1][1], available_parties, eps)
+        available_parties = far_parties
+        unavailable_parties += close_parties
+
+        # Make sure there are still parties available. If not, decrease eps.
+        if len(selected_parties) != len(valid_parties):
+            while len(available_parties) == 0:
+                eps *= 0.8
+                available_parties = unavailable_parties  # Make all remaining parties available again
+                unavailable_parties = []
+    
+                print("Notice: Available parties are too close to selected parties.")
+                print(f"Trying eps = {eps} for party {idx_party+1}")
+                
+                # Make parties unavailable again if they are too close to an already selected party
+                for selected_party in selected_parties:
+                    close_parties, far_parties = organize_parties(selected_party[1], available_parties, eps)
+                    available_parties = far_parties
+                    unavailable_parties += close_parties
+
+    return selected_parties
+
+
+def organize_parties(chosen_party_embedding: np.ndarray, available_parties: list, eps: float = 1.0) -> (list, list):
+    """ Given a chosen party and a list of available parties, separate out the parties that are too close
+    to the chosen party. Close is defined by an embedding on each party such that the 2-norm is within eps.
+    :param chosen_party_embedding: the embedding of the chosen party
+    :param available_parties: the parties that are available to be chosen
+    :param eps: the distance that defines whether two parties are close
+    :return: the parties (with their embedding) that are within eps of the chosen party, and the parties
+    that are further than eps away. If a party is exactly eps from the chosen party, then it's considered far.
+    """
+    close_parties = []
+    far_parties = []
+
+    for party_idx, party_embedding in available_parties:
+        if norm(chosen_party_embedding - party_embedding, ord=2) < eps:
+            close_parties.append((party_idx, party_embedding))
+        else:
+            far_parties.append((party_idx, party_embedding))
+
+    return close_parties, far_parties
