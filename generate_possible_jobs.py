9d77863f3a3e7bcf06862d08686e3eed39f9ac25
Scott Small <ssmall41@gmail.com>
2023-09-20 17:53:14 +0200

    Add Regular runs

--- generate_possible_jobs.py
+++ generate_possible_jobs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def generate_possible_parties(run: str, df_jobs: pd.DataFrame, stat_cols: list, duplicates: bool = False) -> list:
+def generate_possible_parties(run: str, df_jobs: pd.DataFrame, duplicates: bool = False) -> list:
     """ Generate the list of all possible parties given a particular Four Job Fiesta style. These
     are consistent with the definitions on the event pages as of 19.09.23. See:
     https://www.fourjobfiesta.com/help.php
@@ -11,23 +11,28 @@ def generate_possible_parties(run: str, df_jobs: pd.DataFrame, stat_cols: list, 
 
     :param run: the Four Job Fiesta run style ("Regular", "Typhoon", "Volcano", or "Meteor").
     :param df_jobs: the DataFrame of jobs data
-    :param stat_cols: the columns in df_jobs holding the stats
     :param duplicates: flag to allow duplicates. Doesn't do anything for Regular runs.
     :return: the generated jobs
     """
 
     valid_parties = []
 
-    if run == "Meteor":
-        for idx1, job1 in df_jobs[stat_cols].iterrows():
-            for idx2, job2 in df_jobs[stat_cols].iterrows():
-                for idx3, job3 in df_jobs[stat_cols].iterrows():
-                    for idx4, job4 in df_jobs[stat_cols].iterrows():
-                        valid_parties.append([idx1, idx2, idx3, idx4])
+    if run == "Regular":
+        for job1 in df_jobs[df_jobs["Crystal"] == "Wind"].index:
+            for job2 in df_jobs[df_jobs["Crystal"] == "Water"].index:
+                for job3 in df_jobs[df_jobs["Crystal"] == "Fire"].index:
+                    for job4 in df_jobs[df_jobs["Crystal"] == "Earth"].index:
+                        valid_parties.append([job1, job2, job3, job4])
+    elif run == "Meteor":
+        for job1 in df_jobs.index:
+            for job2 in df_jobs.index:
+                for job3 in df_jobs.index:
+                    for job4 in df_jobs.index:
+                        valid_parties.append([job1, job2, job3, job4])
     else:
         raise ValueError(f"Bad game style {run}.")
 
-    if not duplicates:
+    if not duplicates and not run == "Regular":
         valid_parties_no_duplicates = []
         for party in valid_parties:
             if len(party) == len(set(party)):
